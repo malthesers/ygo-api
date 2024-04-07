ffdc16eee25c47af4966f2c49c15ee17e6b78101
malt9830 <malt9830@stud.kea.dk>
2024-04-08 00:41:48 +0200

    Added socials to players

--- interfaces/player.ts
+++ interfaces/player.ts
@@ -4,4 +4,9 @@ export interface IPlayer extends Document {
   name: string
   slug: string
   nationality: string
+  socials?: {
+    youtube: string
+    twitter: string
+    twitch: string
+  }
 }
--- models/player.ts
+++ models/player.ts
@@ -5,6 +5,11 @@ const playerSchema: Schema = new Schema<IPlayer>({
   name: { type: String, required: true },
   slug: { type: String, required: true },
   nationality: { type: String, required: true },
+  socials: {
+    youtube: { type: String },
+    twitter: { type: String },
+    twitch: { type: String },
+  },
 })
 
 const PlayerModel = model<IPlayer>('Player', playerSchema)
