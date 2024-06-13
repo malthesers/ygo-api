82653505846f095733f6acb7dd57ddb9a10f52c8
malt9830 <malt9830@stud.kea.dk>
2024-06-13 21:34:55 +0200

    Added route for specific events

--- routes/events.ts
+++ routes/events.ts
@@ -8,9 +8,40 @@ eventsRouter.get('/', async (req, res) => {
   try {
     const events = await EventModel.find({}).limit(10)
 
-    res.send(events)
+    if (!events) {
+      return res.status(404).send({ message: 'No events found' })
+    }
+
+    return res.send(events)
   } catch (err) {
-    res.status(500).send(err)
+    return res.status(500).send(err)
+  }
+})
+
+eventsRouter.get('/type/:slug', async (req, res) => {
+  const eventTypes = ['wcq', 'ycs', 'team-ycs', 'remote-ycs']
+
+  if (!eventTypes.includes(req.params.slug)) {
+    return res.status(404).send({ message: 'Event type not found' })
+  }
+
+  try {
+    const events = await EventModel.aggregate([
+      { $match: { 'type.slug': req.params.slug } },
+      { $lookup: { from: 'players', localField: 'winner.player', foreignField: '_id', as: 'winner.player' } },
+      { $lookup: { from: 'decks', localField: 'winner.deck', foreignField: '_id', as: 'winner.deck' } },
+      { $lookup: { from: 'decktypes', localField: 'winner.deck.deckType', foreignField: '_id', as: 'deckType' } },
+      { $addFields: { 'winner.deck.deckType': '$deckType' } },
+      { $project: { deckType: 0 } },
+    ])
+
+    if (!events) {
+      return res.status(404).send({ message: `Events of slug "${req.params.slug}" not found` })
+    }
+
+    return res.send(events)
+  } catch (err) {
+    return res.status(500).send(err)
   }
 })
 
@@ -25,7 +56,7 @@ eventsRouter.get('/:slug', async (req, res) => {
       .populate('winner3.deck')
 
     if (!event) {
-      res.status(404).send({ message: `Event of slug "${req.params.slug}" not found` })
+      return res.status(404).send({ message: `Event of slug "${req.params.slug}" not found` })
     }
 
     const decks = await DeckModel.find({ event: event?._id }).populate('deckType').populate('player')
@@ -35,9 +66,9 @@ eventsRouter.get('/:slug', async (req, res) => {
       decks,
     }
 
-    res.send(response)
+    return res.send(response)
   } catch (err) {
-    res.status(500).send(err)
+    return res.status(500).send(err)
   }
 })
 
