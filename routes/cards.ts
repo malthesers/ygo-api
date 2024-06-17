67e8ec4515879e7bc602e9582342491685c2c044
malt9830 <malt9830@stud.kea.dk>
2024-06-17 02:21:28 +0200

    Added route for card including decks

--- routes/cards.ts
+++ routes/cards.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import CardModel from '../models/card'
+import DeckModel from '../models/deck'
 
 const cardsRouter = express.Router()
 
@@ -22,6 +23,33 @@ cardsRouter.get('/:id', async (req, res) => {
     } else {
       return res.status(404).send({ message: `Card with passcode ${req.params.id} not found` })
     }
+  } catch (err) {
+    return res.status(500).send(err)
+  }
+})
+
+cardsRouter.get('/:id/decks', async (req, res) => {
+  try {
+    const card = await CardModel.findOne({ id: req.params.id })
+
+    if (!card) {
+      return res.status(404).send({ message: `Card with passcode ${req.params.id} not found` })
+    }
+
+    const decks = await DeckModel.find({
+      $or: [
+        { 'decklist.mainDeck': { $elemMatch: { card: card._id } } },
+        { 'decklist.extraDeck': { $elemMatch: { card: card._id } } },
+        { 'decklist.sideDeck': { $elemMatch: { card: card._id } } },
+      ],
+    }).populate(['deckType', 'event', 'player'])
+
+    const response = {
+      ...card.toObject(),
+      decks,
+    }
+
+    return res.send(response)
   } catch (err) {
     return res.status(500).send(err)
   }
