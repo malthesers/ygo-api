32f5c0b36ca1fd56207b38799d5ca98801b45b67
malt9830 <malt9830@stud.kea.dk>
2024-06-21 09:10:13 +0200

    Added percentage calculation

--- pipelines/mostPopularCards.ts
+++ pipelines/mostPopularCards.ts
@@ -7,68 +7,85 @@ import { PipelineStage } from 'mongoose'
  * Sorts cards in descending order with most popular first.
  */
 const mostPopularCardsPipeline: PipelineStage[] = [
-  {
-    $match: {
-      decklist: {
-        $exists: true,
-      },
-    },
-  },
-  {
-    $project: {
-      allCards: {
-        $concatArrays: ['$decklist.mainDeck', '$decklist.extraDeck', '$decklist.sideDeck'],
-      },
-    },
-  },
-  {
-    $unwind: '$allCards',
-  },
+  { $match: { decklist: { $exists: true } } },
   {
     $group: {
-      _id: {
-        card: '$allCards.card',
-        deck: '$_id',
-      },
-      count: {
-        $sum: '$allCards.count',
-      },
-    },
-  },
-  {
-    $group: {
-      _id: '$_id.card',
-      deckCount: {
-        $sum: 1,
-      },
-      totalCount: {
-        $sum: '$count',
-      },
+      _id: null,
+      totalDecks: { $sum: 1 },
     },
   },
   {
     $lookup: {
-      from: 'cards',
-      localField: '_id',
-      foreignField: '_id',
-      as: 'details',
+      from: 'decks',
+      let: { totalDecks: '$totalDecks' },
+      pipeline: [
+        { $match: { decklist: { $exists: true } } },
+        {
+          $project: {
+            allCards: {
+              $concatArrays: ['$decklist.mainDeck', '$decklist.extraDeck', '$decklist.sideDeck'],
+            },
+          },
+        },
+        { $unwind: '$allCards' },
+        {
+          $group: {
+            _id: {
+              card: '$allCards.card',
+              deck: '$_id',
+            },
+            count: { $sum: '$allCards.count' },
+          },
+        },
+        {
+          $group: {
+            _id: '$_id.card',
+            deckCount: { $sum: 1 },
+            totalCount: { $sum: '$count' },
+          },
+        },
+        {
+          $lookup: {
+            from: 'cards',
+            localField: '_id',
+            foreignField: '_id',
+            as: 'details',
+          },
+        },
+        { $unwind: '$details' },
+        {
+          $sort: {
+            deckCount: -1,
+            totalCount: -1,
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            details: '$details',
+            deckCount: '$deckCount',
+            totalCount: '$totalCount',
+          },
+        },
+        {
+          $addFields: {
+            percentage: {
+              $multiply: [{ $divide: ['$deckCount', '$$totalDecks'] }, 100],
+            },
+          },
+        },
+      ],
+      as: 'cards',
     },
   },
   {
-    $unwind: '$details',
+    $unwind: '$cards',
   },
   {
-    $sort: {
-      deckCount: -1,
-      totalCount: -1,
-    },
-  },
-  {
-    $project: {
-      _id: 0,
-      details: '$details',
-      deckCount: '$deckCount',
-      totalCount: '$totalCount',
+    $replaceRoot: {
+      newRoot: {
+        $mergeObjects: ['$cards', { totalDecks: '$totalDecks' }],
+      },
     },
   },
 ]
