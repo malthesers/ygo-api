c7d02198baedaa661d5765fa92269f46d6a60755
malt9830 <malt9830@stud.kea.dk>
2024-06-22 22:48:04 +0200

    Updated env variable

--- startup/connect.ts
+++ startup/connect.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose'
 
 export default () => {
-  if (!process.env.MONGODB_URI) {
+  if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI is not defined')
   }
 
   mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error(err))
 }
