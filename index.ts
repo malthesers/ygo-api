335f8277406c29e618491dec681ebe93bedc7ce9
malt9830 <malt9830@stud.kea.dk>
2024-06-23 03:24:46 +0200

    Swagger setup

--- index.ts
+++ index.ts
@@ -1,13 +1,29 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsDoc from 'swagger-jsdoc'
 import connect from './startup/connect'
 import setupRoutes from './startup/routes'
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const swaggerOptions: swaggerJSDoc.Options = {
+  definition: {
+    info: {
+      title: 'YGO API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/*.ts'],
+}
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 connect()
 setupRoutes(app)
